a01742a68f4e4573c76ba83198bfa6eba7a4f349
Nicolas Huyghe <nicolas.huyghe@gadz.org>
2021-03-18 14:53:49 +0100

    Forecast made at ETA

--- ev_Stations_Website/front.py
+++ ev_Stations_Website/front.py
@@ -6,12 +6,11 @@ import geocoder
 import streamlit as st
 import requests
 import datetime
-from datetime import timedelta, time
 from streamlit_folium import folium_static
 from haversine import haversine
 
-map_box_key = 'pk.eyJ1IjoiYmFraTU1IiwiYSI6ImNrbTIyc2J3ZDJhYm8ydnBsb3E2ODByejYifQ.xS8DYo0Hmvf9cIo5ptSeJw'
-ors_key = '5b3ce3597851110001cf624830305b9b81ae4d4483dc86b374a6cc40'
+map_box_key = 'pk.eyJ1Ijoibmh1eWdoZSIsImEiOiJja21lbjUyemUyd2w4MnlrbjMwMXkxMXJoIn0.cBH6JH_95xnmzms6phgI_g'
+ors_key = '5b3ce3597851110001cf6248f94c4c0b04c64137b28f69cbff5fe979'
 
 st.set_page_config(layout="centered")
 
@@ -73,29 +72,39 @@ paris_coord = [48.836743, 2.353442]  # [latitude, longitude]
 
 # Start point
 start_address = st.sidebar.text_input("Start", "Le Wagon Paris")
-geo_start = geocoder.mapbox(start_address, proximity=paris_coord, key=map_box_key)   # Conversion adresse en lat, lng (param proximity permet d'affiner le résultat)
+geo_start = geocoder.mapbox(start_address, proximity=paris_coord, key=map_box_key)
 start_longitude = geo_start.json['lng']
 start_latitude = geo_start.json['lat']
 
 # Destination
-dest_address = st.sidebar.text_input("Destination", "Le Louvre")
+dest_address = st.sidebar.text_input("Destination", "156 rue marcadet, 75018 Paris")
 geo_dest = geocoder.mapbox(dest_address, proximity=paris_coord, key=map_box_key)
 dest_longitude = geo_dest.json['lng']
 dest_latitude = geo_dest.json['lat']
 
 # Date and time
 st.sidebar.markdown("## Schedule your departure")
-date_util = st.sidebar.date_input("Time of departure", datetime.date(2021, 3, 20))
-time_util = st.sidebar.time_input("Heure de prédiction de la disponibilité", datetime.time(hour=20, minute=00))
+
+row2_1, row2_2 = st.sidebar.beta_columns(2)
+with row2_1:
+    date_inputer = st.empty()
+    date_start = date_inputer.date_input("Time of departure", datetime.date(2021, 3, 19))
+with row2_2:
+    time_inputer = st.empty()
+    time_start = time_inputer.time_input("Hour of departure", datetime.time(hour=20, minute=00))
 
 # Terminal choice
-terminal_choice = st.sidebar.selectbox('Select your terminal type', ['Standard charging', 'Fast charging', 'Both'])
+terminal_choice = st.sidebar.radio('Select your terminal type', ['Standard charging', 'Fast charging', 'Both'])
 
-# Option choice
-option_choice = st.sidebar.selectbox("Choose option", [1,2,3])
+# Number of options and option choice
+row2_1, row2_2 = st.sidebar.beta_columns(2)
+with row2_1:
+    nb_option_inputer = st.empty()
+    n = nb_option_inputer.number_input("Number of options", min_value= 3, max_value= 10, step=1)
+with row2_2:
+    option_choice_inputer = st.empty()
+    option_choice = option_choice_inputer.selectbox("Choose option", range(1, n+1))
 
-# Number of options
-n = st.sidebar.number_input("Number of options to display", min_value= 3, max_value= 10, step=1)
 
 ###############################
 #### DISTANCE COMPUTATION #####
@@ -140,68 +149,10 @@ folium.TileLayer('cartodbpositron').add_to(map_directions)
 folium.Marker([start_latitude, start_longitude], icon=folium.Icon(color='darkblue', icon='home')).add_to(map_directions)
 folium.Marker([dest_latitude, dest_longitude], icon=folium.Icon(color='green', icon='glyphicon-screenshot')).add_to(map_directions)
 
-# Add markers for closest stations
-year = date_util.year
-month = date_util.month
-day = date_util.day
-s_id = dist_info['s_id']
-nb_terminals = []
 
-if terminal_choice == 'Standard charging':
-    for i in range(n):
-        resp = requests.get(f'https://ev-stations-docker-image-gcp-2-dzi5homvkq-ew.a.run.app/predict-type?station_id={s_id[i][1:]}&terminal_type=normal&year={year}&month={month}&day={day}&hour={time_util.hour}&minute={time_util.minute}').json()
-        if resp['number_terminals_available'] == 0:
-            number_terminals_available = resp['number_terminals_available']
-            nb_terminals.append(number_terminals_available)
-            folium.Marker([dist_info['latitude'][i], dist_info['longitude'][i]],
-                          tooltip=("No charger available"),
-                          icon=folium.Icon(color='red', icon='glyphicon-remove-circle'))\
-                .add_to(map_directions)
-        elif resp['number_terminals_available'] > 0:
-            number_terminals_available = resp['number_terminals_available']
-            nb_terminals.append(number_terminals_available)
-            folium.Marker([dist_info['latitude'][i], dist_info['longitude'][i]],
-                          tooltip=(f"{number_terminals_available} terminal available"),
-                          icon=folium.Icon(icon='glyphicon-ok-circle'))\
-                .add_to(map_directions)
-
-elif terminal_choice == 'Fast charging':
-    for i in range(n):
-        resp = requests.get(f'https://ev-stations-docker-image-gcp-2-dzi5homvkq-ew.a.run.app/predict-type?station_id={s_id[i][1:]}&terminal_type=fast&year={year}&month={month}&day={day}&hour={time_util.hour}&minute={time_util.minute}').json()
-        if resp['number_terminals_available'] == 0:
-            number_terminals_available = resp['number_terminals_available']
-            nb_terminals.append(number_terminals_available)
-            folium.Marker([dist_info['latitude'][i], dist_info['longitude'][i]],
-                          tooltip=("No charger available"),
-                          icon=folium.Icon(color='red', icon='glyphicon-remove-circle'))\
-                .add_to(map_directions)
-        elif resp['number_terminals_available'] > 0:
-            number_terminals_available = resp['number_terminals_available']
-            nb_terminals.append(number_terminals_available)
-            folium.Marker([dist_info['latitude'][i], dist_info['longitude'][i]],
-                          tooltip=(f"{number_terminals_available} terminal available"),
-                          icon=folium.Icon(icon='glyphicon-ok-circle'))\
-                .add_to(map_directions)
-
-elif terminal_choice == 'Both':
-    for i in range(n):
-        resp = requests.get(f'https://ev-stations-docker-image-gcp-2-dzi5homvkq-ew.a.run.app/predict?station_id={s_id[i][1:]}&year={year}&month={month}&day={day}&hour={time_util.hour}&minute={time_util.minute}').json()
-        if resp['number_terminals_available'] == 0:
-            number_terminals_available = resp['number_terminals_available']
-            nb_terminals.append(number_terminals_available)
-            folium.Marker([dist_info['latitude'][i], dist_info['longitude'][i]],
-                          tooltip=("No charger available"),
-                          icon=folium.Icon(color='red', icon='glyphicon-remove-circle'))\
-                .add_to(map_directions)
-        elif resp['number_terminals_available'] == 1:
-            number_terminals_available = resp['number_terminals_available']
-            nb_terminals.append(number_terminals_available)
-            folium.Marker([dist_info['latitude'][i], dist_info['longitude'][i]],
-                          tooltip=(f"{number_terminals_available} terminal available"),
-                          icon=folium.Icon(icon='glyphicon-ok-circle'))\
-                .add_to(map_directions)
-
-print(closest_stations)
+###############################
+####### COMPUTATION ###########
+###############################
 
 # Compute driving routes
 driving_routes = []
@@ -210,18 +161,12 @@ for i in range(n):
                                    profile='driving-car',
                                    format='geojson'))
 
-
 # Compute walking routes
 walking_routes = []
 for i in range(n):
     walking_routes.append(client.directions(coordinates=[[closest_stations[i][0], closest_stations[i][1]], [dest_longitude, dest_latitude]],
                                   profile='foot-walking',
                                   format='geojson'))
-
-
-###############################
-########### TABLE #############
-###############################
 
 # Computation driving, walking and total times
 times_drive = []
@@ -235,6 +180,72 @@ for i in range(n):
     times_walk.append(t_walk)
     times_total.append(t_total)
 
+
+# Compute ETA
+etas = []
+start_datetime = datetime.datetime.combine(date_start, time_start)
+for i in range(n):
+    etas.append(start_datetime + datetime.timedelta(minutes=times_total[i]))
+
+print(etas[0])
+
+# Add markers for closest stations
+s_id = dist_info['s_id']
+nb_terminals = []
+
+if terminal_choice == 'Standard charging':
+    for i in range(n):
+        resp = requests.get(f'https://ev-stations-docker-image-gcp-2-dzi5homvkq-ew.a.run.app/predict-type?station_id={s_id[i][1:]}&terminal_type=normal&year={etas[i].year}&month={etas[i].month}&day={etas[i].day}&hour={etas[i].hour}&minute={etas[i].minute}').json()
+        if resp['number_terminals_available'] == 0:
+            number_terminals_available = resp['number_terminals_available']
+            nb_terminals.append(number_terminals_available)
+            folium.Marker([dist_info['latitude'][i], dist_info['longitude'][i]],
+                          tooltip=(f"Estimated number of terminal(s) available: {number_terminals_available}"),
+                          icon=folium.Icon(color='red', icon='glyphicon-remove-circle'))\
+                .add_to(map_directions)
+        elif resp['number_terminals_available'] > 0:
+            number_terminals_available = resp['number_terminals_available']
+            nb_terminals.append(number_terminals_available)
+            folium.Marker([dist_info['latitude'][i], dist_info['longitude'][i]],
+                          tooltip=(f"Estimated number of terminal(s) available: {number_terminals_available}"),
+                          icon=folium.Icon(icon='glyphicon-ok-circle'))\
+                .add_to(map_directions)
+
+elif terminal_choice == 'Fast charging':
+    for i in range(n):
+        resp = requests.get(f'https://ev-stations-docker-image-gcp-2-dzi5homvkq-ew.a.run.app/predict-type?station_id={s_id[i][1:]}&terminal_type=fast&year={etas[i].year}&month={etas[i].month}&day={etas[i].day}&hour={etas[i].hour}&minute={etas[i].minute}').json()
+        if resp['number_terminals_available'] == 0:
+            number_terminals_available = resp['number_terminals_available']
+            nb_terminals.append(number_terminals_available)
+            folium.Marker([dist_info['latitude'][i], dist_info['longitude'][i]],
+                          tooltip=(f"Estimated number of terminal(s) available: {number_terminals_available}"),
+                          icon=folium.Icon(color='red', icon='glyphicon-remove-circle'))\
+                .add_to(map_directions)
+        elif resp['number_terminals_available'] > 0:
+            number_terminals_available = resp['number_terminals_available']
+            nb_terminals.append(number_terminals_available)
+            folium.Marker([dist_info['latitude'][i], dist_info['longitude'][i]],
+                          tooltip=(f"Estimated number of terminal(s) available: {number_terminals_available}"),
+                          icon=folium.Icon(icon='glyphicon-ok-circle'))\
+                .add_to(map_directions)
+
+elif terminal_choice == 'Both':
+    for i in range(n):
+        resp = requests.get(f'https://ev-stations-docker-image-gcp-2-dzi5homvkq-ew.a.run.app/predict?station_id={s_id[i][1:]}&year={etas[i].year}&month={etas[i].month}&day={etas[i].day}&hour={etas[i].hour}&minute={etas[i].minute}').json()
+        if resp['number_terminals_available'] == 0:
+            number_terminals_available = resp['number_terminals_available']
+            nb_terminals.append(number_terminals_available)
+            folium.Marker([dist_info['latitude'][i], dist_info['longitude'][i]],
+                          tooltip=(f"Estimated number of terminal(s) available: {number_terminals_available}"),
+                          icon=folium.Icon(color='red', icon='glyphicon-remove-circle'))\
+                .add_to(map_directions)
+        elif resp['number_terminals_available'] == 1:
+            number_terminals_available = resp['number_terminals_available']
+            nb_terminals.append(number_terminals_available)
+            folium.Marker([dist_info['latitude'][i], dist_info['longitude'][i]],
+                          tooltip=(f"Estimated number of terminal(s) available: {number_terminals_available}"),
+                          icon=folium.Icon(icon='glyphicon-ok-circle'))\
+                .add_to(map_directions)
 
 # Define index order given number of terminals available and total time
 sort_index = []
@@ -253,6 +264,7 @@ walking_routes_sorted = []
 times_drive_sorted = []
 times_walk_sorted = []
 times_total_sorted = []
+etas_sorted = []
 nb_terminals_sorted = []
 for i in sort_index:
     driving_routes_sorted.append(driving_routes[i])
@@ -260,14 +272,8 @@ for i in sort_index:
     times_drive_sorted.append(times_drive[i])
     times_walk_sorted.append(times_walk[i])
     times_total_sorted.append(times_total[i])
+    etas_sorted.append(etas[i])
     nb_terminals_sorted.append(nb_terminals[i])
-
-
-# Compute ETA
-etas = []
-td_start = timedelta(hours=time_util.hour, minutes=time_util.minute)
-for i in range(n):
-    etas.append(td_start + timedelta(minutes=times_total_sorted[i]))
 
 
 ###############################
@@ -280,23 +286,18 @@ def style_function(color):
                                 opacity=0.5,
                                 weight=4)
 
-def strfdelta(tdelta):
-    hours, remainder = divmod(tdelta.seconds, 3600)
-    minutes, seconds = divmod(remainder, 60)
-    return f'{hours}:{minutes}'
-
+st.markdown(f"Your estimated time of arrival is **{etas_sorted[option_choice-1].hour}:{etas_sorted[option_choice-1].minute}**.\
+    To get to your destination, you will have {times_drive_sorted[option_choice-1]}min car :car: and {times_walk_sorted[option_choice-1]}min walk :walking:")
 
 folium.GeoJson(driving_routes_sorted[option_choice-1], name='driving_route').add_to(map_directions)
 folium.GeoJson(walking_routes_sorted[option_choice-1], name='walking_route', style_function=style_function('green')).add_to(map_directions)
-st.write(f"Your estimated time of arrival is {strfdelta(etas[option_choice-1])}")
-
 
 dict_df = {'Option': [f'Option {i+1}' for i in range(n)],
             'Drive (in min)': times_drive_sorted,
             'Walk (in min)': times_walk_sorted,
             'Total (in min)': times_total_sorted,
             'Est. nb term.': nb_terminals_sorted,
-            'ETA': [strfdelta(eta) for eta in etas]}
+            'ETA': [f'{eta.hour}:{eta.minute}' for eta in etas_sorted]}
 
 df_stations = pd.DataFrame(data=dict_df).set_index('Option')
 
